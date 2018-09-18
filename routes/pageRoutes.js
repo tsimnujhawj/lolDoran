524228cf5ef5feb76bf6b82d5f1cc2fb3cfad451
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-09-18 01:01:25 -0500

    fix

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,5 +1,5 @@
-// const express = require("express");
-// const router = express.Router();
+const request = require("request")
+const cheerio = require("cheerio")
 
 module.exports = (app)=>{
 // const playerName = "TigerBang"; // for testing purposes
@@ -7,5 +7,15 @@ module.exports = (app)=>{
 // TODO: setup home page hbs view
 app.get("/", (req, res)=> res.render("index", {champName: "SELECT A CHAMP"}));
 
-app.get(`/champion`, (req, res)=> res.render("champion"));
+app.get(`/champion`, (req, res)=>{
+    console.log(req.body)
+    const info = request(`https://www.probuilds.net/champions/details/${req.body.champName}`, (err, res, html)=>{
+        if (err) throw err
+        const $ = cheerio.load(html);
+        const popSection = $(".popular-section");
+        return popSection
+    })
+}
+);
+
 };
--- routes/reqRoutes.js
+++ routes/reqRoutes.js
@@ -5,33 +5,39 @@ const cheerio = require("cheerio")
 // res.render(`/champion`, {itemName: itemName})
 
 module.exports = (app)=>{
-    app.post("/", (req, res)=>{
-        const champName = req.body.champName;
-        // make http request to riot api and scrap probuilds for champ
-        request(`https://www.probuilds.net/champions/details/${champName}`, (err, res, html)=>{
-            if (err) throw err
-            const $ = cheerio.load(html);
-            const popSection = $(".popular-section");
-            // res.render("/champion", {itemName: popSection});
+    app.post("/search", (req, res, next)=>{
+        console.log(req.body.champName)
+        return next()
+    },
+    (req, res)=>{
+        const champName = req.body.champName
+        let result = {};
+            request(`https://www.probuilds.net/champions/details/${champName}`, (err, res, html)=>{
+                if (err) throw err
+                const $ = cheerio.load(html);
+                const popSection = $(".popular-section");
+                result.popSection = popSection
+                db.doransbook.create(result).then((data)=>{
+                    console.log(data);
+                })
+            })
+            console.log(result.popSection);
+    }
+);
+    
+
+    app.get("/homey", (req, res)=> res.send("Hello Home!"));
+};
 
             // the name of items and spells
             // $(".bigData .item-name").each((index, el)=>{
             //     const itemName = $(el).text();
-            //     console.log(itemName)
             // })
             // // the percent of each item/spells
             // $(".bigData .green").each((index, el)=>{
             //     const popPercent = $(el).text();
-            //     console.log(popPercent)
             // })
             // // the image of each item/spells
             // $(".bigData .item").each((index, el)=>{
             //     const itemImg = $(el).children("img").attr("src");
-            //     console.log(itemImg);
             // })
-        })
-        
-    });
-    
-    app.get("/homey", (req, res)=> res.send("Hello Home!"));
-};
