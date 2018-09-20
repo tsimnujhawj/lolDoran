1841673a4a47df4fed4edbfdb5d7611c37f2fd19
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-09-20 16:51:35 -0500

    added request promise and got scraped info to display into view

--- routes/reqRoutes.js
+++ routes/reqRoutes.js
@@ -3,6 +3,7 @@
 const request = require("request")
 const cheerio = require("cheerio")
 const db = require("../models/champions")
+const rp = require('request-promise');
 
 // write items
 // res.render(`/champion`, {itemName: itemName})
@@ -16,68 +17,28 @@ module.exports = (app)=>{
     (req, res)=>{
         const champName = req.body.champName
         let result = {};
-            request(`https://www.probuilds.net/champions/details/${champName}`, (err, res, html)=>{
+            rp(`https://www.probuilds.net/champions/details/${champName}`, (err, res, html)=>{
                 if (err) throw err
                 const $ = cheerio.load(html);
                 const popSection = $(".popular-section").html();
-                
-                // TODO: Finish this insert into MongoDB
-                // let data = {
-                //     name: req.body.champName,
-                //     topItems: {
-                //         item: {
-                //             name: "this.itemName",
-                //             imgLink: "this.itemImg",
-                //             percent: 99
-                //         }
-                //     }
-                // }
-
             // the name of items and spells
             $(".bigData .item-name").each((index, el)=>{
                 const itemName = $(el).text();
-                // db.create({
-                //     topItems: {
-                //         item: {
-                //             name: itemName
-                //         }
-                //     }
-                // }).then((info)=>{
-                //     console.log(info)
-                // })
-                // $(itemName).appendTo('.popItems')
-                // $.html()
             })
             // // the percent of each item/spells
             $(".bigData .green").each((index, el)=>{
                 const popPercent = $(el).text();
-                // db.create({
-                //     topItems: {
-                //         item: {
-                //             percent: popPercent
-                //         }
-                //     }
-                // }).then((info)=>{
-                //     console.log(info)
-                // })
             })
             // // the image of each item/spells
             $(".bigData .item").each((index, el)=>{
                 const itemImg = $(el).children("img").attr("src");
-                // db.create({
-                //     topItems: {
-                //         item: {
-                //             imgLink: itemImg
-                //         }
-                //     }
-                // }).then((info)=>{
-                //     console.log(info)
-                // })
             })
-                
-            })
-            const response = res.json()
-            
+        }).then((data)=>{
+            console.log(data);
+            const $ = cheerio.load(data);
+            const textData = $(".popular-section").html();
+            res.render("index", {stuff: textData});
+        })
     }
 );
     
