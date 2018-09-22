151be33bbf5b41aa5b7d0626634c73a49749cdb3
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-09-22 12:01:46 -0500

    fixed dropdown

--- routes/reqRoutes.js
+++ routes/reqRoutes.js
@@ -4,6 +4,24 @@ const rp = require("request-promise");
 const Post = require("../models/forumPost")
 const moment = require("moment")
 
+const champArr = ["Ahri", "Akali", "Alistar", "Amumu", "Anivia",
+"Annie", "Ashe", "Blitzcrank", "Brand", "Caitlyn", "Cassiopeia",
+"Cho'gath", "Corki", "Darius", "Dr. Mundo", "Ekko", "Evelynn", "Ezreal",
+"Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen",
+"Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna",
+"Jarvan IV", "Jax", "Karma", "Karthus", "Kassadin", "Katarina",
+"Kayle", "Kennen", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona",
+"Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi",
+"Miss Fortune", "Mordekaiser", "Morgana", "Nasus", "Nautilus",
+"Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon",
+"Poppy", "Rammus", "Renekton", "Riven", "Rumble", "Ryze",
+"Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir",
+"Skarner", "Sona", "Soraka", "Swain", "Talon", "Taric", "Teemo",
+"Tristana", "Trundle", "Trydamere", "Twisted Fate", "Twitch",
+"Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Viktor", "Vladimir",
+"Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo",
+"Yorick", "Zed", "Ziggs", "Zilean"]
+
 
 module.exports = (app)=>{
     app.post("/", (req, res, next)=>{
